998090e1d0afc5d12ef6f38168041a28f7cc2c71
VatoDev <bigmalv@gmail.com>
2024-09-15 21:40:59 -0600

    waaa

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,15 +1,23 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { useState } from 'react'; // Para manejar el estado de carga de la imagen
 import '../styles/ProductCard.css';
 
 function ProductCard({ product }) {
+    const [isImageLoaded, setImageLoaded] = useState(false); // Estado para la carga de la imagen
+
     return (
         <div className="product-card">
+            {/* Añadir un placeholder o spinner mientras la imagen se carga */}
+            {!isImageLoaded && <div className="image-placeholder">Cargando...</div>}
+
             <img
-                src={`${product.image}`}
-                alt={product.name}
-                className="product-image"
+                src={product.image}
+                alt={`Imagen del producto ${product.name}`}
+                className={`product-image ${isImageLoaded ? '' : 'hidden'}`}
+                onLoad={() => setImageLoaded(true)} // Actualiza el estado una vez que la imagen esté cargada
             />
+
             <div className="product-info">
                 <h2 className="product-name">{product.name}</h2>
                 <p className="product-description">{product.description}</p>
